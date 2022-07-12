d6034f3658a174d89f154e918957c12c28617e0f
DimasRP <drizki80@gmail.com>
2022-07-12 17:17:07 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const myCar = ["ayla",1,"brio",2,"avanza",3,"sigra"];
+const types = []
+for (let i = 0; i < myCar.length; i++){
+    types.push(typeof myCar[i])
+}
 
-
-// console.log(types);
+console.log(types);
 
 for (let i = 1; i <= 5; i++){
     console.log(`---keliling ke ${i}`);
